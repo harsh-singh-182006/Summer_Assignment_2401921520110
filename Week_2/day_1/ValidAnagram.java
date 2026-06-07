6f3d862d4055674b0119ed7374e9e61474be937c
Harsh Singh <harshsingh1822006@gmail.com>
2026-06-08 03:11:02 +0530

    Valid Anagram

---
+++ Week_2/day_1/ValidAnagram.java
@@ -0,0 +1,21 @@
+package Week_2.day_1;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            count[ch - 'a']--;
+        }
+
+        // Check if all elements in the array are 0
+        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+        return allZeros;
+    }
+}
