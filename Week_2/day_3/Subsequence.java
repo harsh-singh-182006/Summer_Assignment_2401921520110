ca6c41a9f84d30dda916d54741eaa0a844159a89
Harsh Singh <harshsingh1822006@gmail.com>
2026-06-11 00:32:02 +0530

    Is Subsequence

---
+++ Week_2/day_3/Subsequence.java
@@ -0,0 +1,18 @@
+package Week_2.day_3;
+
+public class Subsequence {
+     public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    
+    }
+}
